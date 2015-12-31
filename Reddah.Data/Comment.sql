c56f55a15b41dda209561abd0da28684b95277b8
bailin.wei <bailin.wei@thomsonreuters.com>
2015-12-31 21:16:02 +0000

    20160101-Add missing files

---
+++ Reddah.Data/Comment.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Comment]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [ParentId] INT NULL, 
+    [ParentType] CHAR(10) NULL, 
+    [Content] NCHAR(10) NULL, 
+    [Up] INT NULL DEFAULT 0, 
+    [Down] INT NULL DEFAULT 0
+)
