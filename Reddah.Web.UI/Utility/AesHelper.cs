a98db7f51235f97f6b05dc255b919ddb0a6ae23f
bailin.wei <weibailin@hotmail.com>
2016-07-05 05:43:01 +0800

    Add AES encryption utility

---
+++ Reddah.Web.UI/Utility/AesHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace Reddah.Web.UI.Utility
+{
+    using System;
+    using System.Text;
+
+    public static class AesHelper
+    {
+        public static string Encrypt(string text, string aesKey, string aesIV)
+        {
+            return AesProvider.Encrypt(text, Encoding.UTF8.GetBytes(aesKey), Encoding.UTF8.GetBytes(aesIV));
+        }
+
+        public static string Decrypt(string textCipher, string aesKey, string aesIV)
+        {
+            return AesProvider.Decrypt(textCipher, Encoding.UTF8.GetBytes(aesKey), Encoding.UTF8.GetBytes(aesIV));
+        }
+    }
+}
---
+++ Reddah.Web.UI/Utility/AesProvider.cs
@@ -0,0 +1,84 @@
+﻿namespace Reddah.Web.UI.Utility
+{
+    using System;
+    using System.IO;
+    using System.Security.Cryptography;
+
+    public class AesProvider
+    {
+        public static string Encrypt(string text, byte[] key, byte[] iv)
+        {
+            if (String.IsNullOrEmpty(text))
+                return String.Empty;
+
+            byte[] encryptedUserInfo;
+
+            try
+            {
+                // Create an AesCryptoServiceProvider object with the specified key and IV.
+                using (var aesAlg = new AesCryptoServiceProvider())
+                {
+                    aesAlg.Key = key;
+                    aesAlg.IV = iv;
+
+                    // Create a decrytor to perform the stream transform.
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                    // Create the streams used for encryption.
+                    using (var msEncrypt = new MemoryStream())
+                    {
+                        using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (var swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                //Write all data to the stream.
+                                swEncrypt.Write(text);
+                            }
+                            encryptedUserInfo = msEncrypt.ToArray();
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                return String.Empty;
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return Convert.ToBase64String(encryptedUserInfo);
+        }
+
+        public static string Decrypt(string textCipher, byte[] key, byte[] iv)
+        {
+            // Declare the string used to hold the decrypted text.
+            string userInfoString = String.Empty;
+
+            if (String.IsNullOrEmpty(textCipher))
+                return userInfoString;
+
+            // Create an AesCryptoServiceProvider object with the specified key and IV.
+            using (var aesAlg = new AesCryptoServiceProvider())
+            {
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                // Create a decrytor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (var msDecrypt = new MemoryStream(Convert.FromBase64String(textCipher)))
+                {
+                    using (var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            // Read the decrypted bytes from the decrypting stream and place them in a string.
+                            userInfoString = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return userInfoString;
+        }
+    }
+}
