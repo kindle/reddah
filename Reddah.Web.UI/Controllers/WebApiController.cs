81a09e9f0ffc42e90871aa0539bae19f0775d4ab
kindle <weibailin@hotmail.com>
2019-01-13 19:48:39 +0800

    change webapi to post method for testing

--- Reddah.Web.UI/Controllers/WebApiController.cs
+++ Reddah.Web.UI/Controllers/WebApiController.cs
@@ -21,14 +21,14 @@
         /// Get Articles
         /// </summary>
         /// <returns>list of posts</returns>
-        [HttpGet]
-        public IEnumerable<ArticlePreview> GetArticles(int[] loadedIds)
+        [HttpPost]
+        public IEnumerable<ArticlePreview> GetArticles([FromBody] int[] loadedIds)
         {
             IEnumerable<ArticlePreview> result = null;
 
             UserProfileModel userProfileModel = new UserProfileModel();
             userProfileModel.LoadedIds = loadedIds;
-            userProfileModel.Locale = "zh-cn";
+            userProfileModel.Locale = "en-us";
             userProfileModel.Menu = "new";
 
             result = new UserProfileArticleViewModel(userProfileModel).Articles;
