d573c7971f321239e1cc421c59399a01df6433b2
kindle <weibailin@hotmail.com>
2019-02-28 17:31:05 +0800

    add webapi 4.7 project for login.reddah.com

---
+++ Reddah.Web.Login/Controllers/AuthController.cs
@@ -0,0 +1,164 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Http;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+namespace Reddah.Web.Login.Controllers
+{
+    [RoutePrefix("api/auth")]
+    public class AuthController : ApiController
+    {
+        public const string SecretKey = "abc1234567@reddahcom";
+
+        [Route("login")]
+        public IHttpActionResult Login([FromBody]UserModel user)
+        {
+            if (user == null)
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            if (user.UserName == "johndoe" && user.Password == "def@123")
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+                var claims = new List<Claim>();
+                claims.Add(new Claim("100","view"));
+                claims.Add(new Claim("101", "post"));
+
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: "https://login.reddah.com",
+                    audience: "wind",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(60),
+                    signingCredentials: signinCredentials
+                );
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+                return Ok(new { Token = tokenString });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        [Route("valid1")]
+        public IHttpActionResult ValidSignature([FromBody]Product product)
+        {
+            if (product.Name == null)
+            {
+                return BadRequest("Invalid client request1");
+            }
+
+            string token = product.Name;
+            string[] parts = token.Split('.');
+            string header = parts[0];
+            string payload = parts[1];
+            byte[] crypto = Base64UrlDecode(parts[2]);
+
+            string headerJson = Encoding.UTF8.GetString(Base64UrlDecode(header));
+            JObject headerData = JObject.Parse(headerJson);
+
+            string payloadJson = Encoding.UTF8.GetString(Base64UrlDecode(payload));
+            JObject payloadData = JObject.Parse(payloadJson);
+            
+            try
+            {
+                var keyBytes = Encoding.UTF8.GetBytes(SecretKey); // your key here
+
+                AsymmetricKeyParameter asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
+                RsaKeyParameters rsaKeyParameters = (RsaKeyParameters)asymmetricKeyParameter;
+                RSAParameters rsaParameters = new RSAParameters();
+                rsaParameters.Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned();
+                rsaParameters.Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned();
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.ImportParameters(rsaParameters);
+
+                SHA256 sha256 = SHA256.Create();
+                byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(parts[0] + '.' + parts[1]));
+
+                RSAPKCS1SignatureDeformatter rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+                rsaDeformatter.SetHashAlgorithm("SHA256");
+                if (!rsaDeformatter.VerifySignature(hash, Convert.FromBase64String(parts[2])))
+                    throw new ApplicationException(string.Format("Invalid signature"));
+            }
+            catch(Exception ex)
+            {
+                return Ok(new { Result = ex.Message.ToString() });
+            }
+
+            return Ok(new { Result = payloadData.ToString() });
+        }
+
+        private static byte[] Base64UrlDecode(string input)
+        {
+            var output = input;
+            output = output.Replace('-', '+'); // 62nd char of encoding
+            output = output.Replace('_', '/'); // 63rd char of encoding
+            switch (output.Length % 4) // Pad with trailing '='s
+            {
+                case 0: break; // No pad chars in this case
+                case 1: output += "==="; break; // Three pad chars
+                case 2: output += "=="; break; // Two pad chars
+                case 3: output += "="; break; // One pad char
+                default: throw new Exception("Illegal base64url string!");
+            }
+            var converted = Convert.FromBase64String(output); // Standard base64 decoder
+            return converted;
+        }
+
+        [Route("valid2")]
+        [HttpGet]
+        public IHttpActionResult ValidSignature2()
+        {
+            string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyIxMDAiOiJ2aWV3IiwiMTAxIjoicG9zdCIsImV4cCI6MTU1MTM0Nzk1MywiaXNzIjoiaHR0cHM6Ly9sb2dpbi5yZWRkYWguY29tIiwiYXVkIjoid2luZCJ9.j71cqJin-aQmDL0AEZQoTjpZrO3Gd3Ohnh_efSzlA8g";
+            string[] parts = token.Split('.');
+            string header = parts[0];
+            string payload = parts[1];
+            byte[] crypto = Base64UrlDecode(parts[2]);
+
+            string headerJson = Encoding.UTF8.GetString(Base64UrlDecode(header));
+            JObject headerData = JObject.Parse(headerJson);
+
+            string payloadJson = Encoding.UTF8.GetString(Base64UrlDecode(payload));
+            JObject payloadData = JObject.Parse(payloadJson);
+
+            try
+            {
+                var keyBytes = Encoding.UTF8.GetBytes(SecretKey); // your key here
+
+                AsymmetricKeyParameter asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
+                RsaKeyParameters rsaKeyParameters = (RsaKeyParameters)asymmetricKeyParameter;
+                RSAParameters rsaParameters = new RSAParameters();
+                rsaParameters.Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned();
+                rsaParameters.Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned();
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                rsa.ImportParameters(rsaParameters);
+
+                SHA256 sha256 = SHA256.Create();
+                byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(parts[0] + '.' + parts[1]));
+
+                RSAPKCS1SignatureDeformatter rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
+                rsaDeformatter.SetHashAlgorithm("SHA256");
+                if (!rsaDeformatter.VerifySignature(hash, Convert.FromBase64String(parts[2])))
+                    throw new ApplicationException(string.Format("Invalid signature"));
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { Result = ex.Message.ToString() });
+            }
+
+            return Ok(new { Result = payloadData.ToString() });
+        }
+    }
+}
---
+++ Reddah.Web.Login/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Reddah.Web.Login.WebApiApplication" Language="C#" %>
