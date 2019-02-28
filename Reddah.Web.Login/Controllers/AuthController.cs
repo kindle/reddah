29f0647095c35eca832540aa3fb05a883a9c83d2
kindle <weibailin@hotmail.com>
2019-03-01 07:49:02 +0800

    complete sign/verify basic jwt features

--- Reddah.Web.Login/Controllers/AuthController.cs
+++ Reddah.Web.Login/Controllers/AuthController.cs
@@ -4,12 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
-using System.Security.Cryptography;
 using System.Text;
 using System.Web.Http;
-using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.Security;
 
 namespace Reddah.Web.Login.Controllers
 {
@@ -18,8 +14,8 @@ namespace Reddah.Web.Login.Controllers
     {
         public const string SecretKey = "abc1234567@reddahcom";
 
-        [Route("login")]
-        public IHttpActionResult Login([FromBody]UserModel user)
+        [Route("sign")]
+        public IHttpActionResult Sign([FromBody]UserModel user)
         {
             if (user == null)
             {
@@ -37,9 +33,9 @@ namespace Reddah.Web.Login.Controllers
 
                 var tokeOptions = new JwtSecurityToken(
                     issuer: "https://login.reddah.com",
-                    audience: "wind",
+                    audience: user.UserName,
                     claims: claims,
-                    expires: DateTime.Now.AddMinutes(60),
+                    expires: DateTime.Now.AddMinutes(1),
                     signingCredentials: signinCredentials
                 );
 
@@ -51,114 +47,46 @@ namespace Reddah.Web.Login.Controllers
                 return Unauthorized();
             }
         }
-
-        [Route("valid1")]
-        public IHttpActionResult ValidSignature([FromBody]Product product)
+        [Route("verify")]
+        [HttpPost]
+        public IHttpActionResult Verify([FromBody]Product product)
         {
-            if (product.Name == null)
-            {
-                return BadRequest("Invalid client request1");
-            }
-
-            string token = product.Name;
-            string[] parts = token.Split('.');
-            string header = parts[0];
-            string payload = parts[1];
-            byte[] crypto = Base64UrlDecode(parts[2]);
-
-            string headerJson = Encoding.UTF8.GetString(Base64UrlDecode(header));
-            JObject headerData = JObject.Parse(headerJson);
-
-            string payloadJson = Encoding.UTF8.GetString(Base64UrlDecode(payload));
-            JObject payloadData = JObject.Parse(payloadJson);
+            //string signedAndEncodedToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyIxMDAiOiJ2aWV3IiwiMTAxIjoicG9zdCIsImV4cCI6MTU1MTM5NjE5MiwiaXNzIjoiaHR0cHM6Ly9sb2dpbi5yZWRkYWguY29tIiwiYXVkIjoid2luZCJ9.bqVbbDRbw1o_bS0AZiU5hdb4EcBREoqzw1o-8zd3TrE";
             
+            //expired:
+            //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyIxMDAiOiJ2aWV3IiwiMTAxIjoicG9zdCIsImV4cCI6MTU1MTM5NTExMywiaXNzIjoiaHR0cHM6Ly9sb2dpbi5yZWRkYWguY29tIiwiYXVkIjoiam9obmRvZSJ9.SXayamY0LV_tptXLnW7Onjd5hSrvoRQM2JAPlIGrHBc
+
+            string signedAndEncodedToken = product.Name;
+            var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                RequireExpirationTime = true,
+                RequireSignedTokens = true,
+                ValidateAudience = false,
+                ValidateIssuer = true,
+                ValidIssuers = new string[]
+                {
+                    "https://login.reddah.com",
+                    "http://my.othertokenissuer.com"
+                },
+                ValidateLifetime = true,
+
+                IssuerSigningKey = signingKey
+            };
+
+            SecurityToken validatedToken;
+            var tokenHandler = new JwtSecurityTokenHandler();
             try
             {
-                var keyBytes = Encoding.UTF8.GetBytes(SecretKey); // your key here
-
-                AsymmetricKeyParameter asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
-                RsaKeyParameters rsaKeyParameters = (RsaKeyParameters)asymmetricKeyParameter;
-                RSAParameters rsaParameters = new RSAParameters();
-                rsaParameters.Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned();
-                rsaParameters.Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned();
-                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-                rsa.ImportParameters(rsaParameters);
-
-                SHA256 sha256 = SHA256.Create();
-                byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(parts[0] + '.' + parts[1]));
-
-                RSAPKCS1SignatureDeformatter rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
-                rsaDeformatter.SetHashAlgorithm("SHA256");
-                if (!rsaDeformatter.VerifySignature(hash, Convert.FromBase64String(parts[2])))
-                    throw new ApplicationException(string.Format("Invalid signature"));
+                tokenHandler.ValidateToken(signedAndEncodedToken, tokenValidationParameters, out validatedToken);
             }
             catch(Exception ex)
             {
-                return Ok(new { Result = ex.Message.ToString() });
+                return Ok(new { Result = "Invalid Signature:" + ex.Message.ToString() });
             }
 
-            return Ok(new { Result = payloadData.ToString() });
-        }
-
-        private static byte[] Base64UrlDecode(string input)
-        {
-            var output = input;
-            output = output.Replace('-', '+'); // 62nd char of encoding
-            output = output.Replace('_', '/'); // 63rd char of encoding
-            switch (output.Length % 4) // Pad with trailing '='s
-            {
-                case 0: break; // No pad chars in this case
-                case 1: output += "==="; break; // Three pad chars
-                case 2: output += "=="; break; // Two pad chars
-                case 3: output += "="; break; // One pad char
-                default: throw new Exception("Illegal base64url string!");
-            }
-            var converted = Convert.FromBase64String(output); // Standard base64 decoder
-            return converted;
-        }
-
-        [Route("valid2")]
-        [HttpGet]
-        public IHttpActionResult ValidSignature2()
-        {
-            string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyIxMDAiOiJ2aWV3IiwiMTAxIjoicG9zdCIsImV4cCI6MTU1MTM0Nzk1MywiaXNzIjoiaHR0cHM6Ly9sb2dpbi5yZWRkYWguY29tIiwiYXVkIjoid2luZCJ9.j71cqJin-aQmDL0AEZQoTjpZrO3Gd3Ohnh_efSzlA8g";
-            string[] parts = token.Split('.');
-            string header = parts[0];
-            string payload = parts[1];
-            byte[] crypto = Base64UrlDecode(parts[2]);
-
-            string headerJson = Encoding.UTF8.GetString(Base64UrlDecode(header));
-            JObject headerData = JObject.Parse(headerJson);
-
-            string payloadJson = Encoding.UTF8.GetString(Base64UrlDecode(payload));
-            JObject payloadData = JObject.Parse(payloadJson);
-
-            try
-            {
-                var keyBytes = Encoding.UTF8.GetBytes(SecretKey); // your key here
-
-                AsymmetricKeyParameter asymmetricKeyParameter = PublicKeyFactory.CreateKey(keyBytes);
-                RsaKeyParameters rsaKeyParameters = (RsaKeyParameters)asymmetricKeyParameter;
-                RSAParameters rsaParameters = new RSAParameters();
-                rsaParameters.Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned();
-                rsaParameters.Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned();
-                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-                rsa.ImportParameters(rsaParameters);
-
-                SHA256 sha256 = SHA256.Create();
-                byte[] hash = sha256.ComputeHash(Encoding.UTF8.GetBytes(parts[0] + '.' + parts[1]));
-
-                RSAPKCS1SignatureDeformatter rsaDeformatter = new RSAPKCS1SignatureDeformatter(rsa);
-                rsaDeformatter.SetHashAlgorithm("SHA256");
-                if (!rsaDeformatter.VerifySignature(hash, Convert.FromBase64String(parts[2])))
-                    throw new ApplicationException(string.Format("Invalid signature"));
-            }
-            catch (Exception ex)
-            {
-                return Ok(new { Result = ex.Message.ToString() });
-            }
-
-            return Ok(new { Result = payloadData.ToString() });
+            return Ok(new { Result = validatedToken.ToString() });
         }
     }
 }
