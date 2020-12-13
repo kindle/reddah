972e620fb4cb8f9eda150102b0d757a0236fd138
bailin wei <weibailin@hotmail.com>
2020-12-14 06:11:12 +0800

    first version in ios for metronome

--- Reddah.Metronome/src/app/app.component.ts
+++ Reddah.Metronome/src/app/app.component.ts
@@ -8,68 +8,67 @@ import { Globalization } from '@ionic-native/globalization/ngx';
 
 
 import {
-  Plugins,
-  StatusBarStyle,
-  Capacitor,
+    Plugins,
+    StatusBarStyle,
+    Capacitor,
 } from '@capacitor/core';
 
 const { App, StatusBar, SplashScreen, Geolocation } = Plugins;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private reddah: ReddahService,
-    private localStorageService: LocalStorageService,
+    constructor(
+        private platform: Platform,
+        private reddah: ReddahService,
+        private localStorageService: LocalStorageService,
+        private globalization: Globalization,
+    ) {
+        this.initializeApp();
+    }
 
-    private globalization: Globalization,
-  ) {
-      this.initializeApp();
-  }
-
-  initializeApp() {
-    this.platform.ready().then(() => {
-      
-      if(this.reddah.isMobile()){
-          StatusBar.hide();
-      }
-      this.reddah.init();
-      let currentLocale = this.localStorageService.retrieve("Reddah_Locale");
-      let defaultLocale ="en-US"
-      if(currentLocale==null){
-        if(this.reddah.isMobile())
-        { 
-            this.globalization.getPreferredLanguage()
-            .then(res => {
-                let localeStr = this.reddah.fixLocaleStr(res.value);
-                if(this.reddah.Locales.filter(l=>l.Name==localeStr).length>0)
-                {
-                  this.localStorageService.store("Reddah_Locale", localeStr);
-                  this.reddah.loadTranslate(localeStr);
-                }
-                else{
+    initializeApp() {
+        this.platform.ready().then(() => {
+            SplashScreen.hide();
+            if(this.reddah.isMobile()){
+                StatusBar.hide();
+            }
+            this.reddah.init();
+            let currentLocale = this.localStorageService.retrieve("Reddah_Locale");
+            let defaultLocale ="en-US"
+            if(currentLocale==null){
+              if(this.reddah.isMobile())
+              { 
+                  this.globalization.getPreferredLanguage()
+                  .then(res => {
+                      let localeStr = this.reddah.fixLocaleStr(res.value);
+                      if(this.reddah.Locales.filter(l=>l.Name==localeStr).length>0)
+                      {
+                        this.localStorageService.store("Reddah_Locale", localeStr);
+                        this.reddah.loadTranslate(localeStr);
+                      }
+                      else{
+                        this.localStorageService.store("Reddah_Locale", defaultLocale);
+                        this.reddah.loadTranslate(defaultLocale);
+                      }
+                  })
+                  .catch(e => {
+                      this.localStorageService.store("Reddah_Locale", defaultLocale);
+                      this.reddah.loadTranslate(defaultLocale);
+                  });
+              }
+              else{
                   this.localStorageService.store("Reddah_Locale", defaultLocale);
                   this.reddah.loadTranslate(defaultLocale);
-                }
-            })
-            .catch(e => {
-                this.localStorageService.store("Reddah_Locale", defaultLocale);
-                this.reddah.loadTranslate(defaultLocale);
-            });
-        }
-        else{
-            this.localStorageService.store("Reddah_Locale", defaultLocale);
-            this.reddah.loadTranslate(defaultLocale);
-        }
+              }
 
-      }
-      else{
-        this.reddah.loadTranslate(currentLocale);
-      }
-    });
-  }
+            }
+            else{
+              this.reddah.loadTranslate(currentLocale);
+            }
+        });
+    }
 }
