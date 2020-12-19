12431316142dfd9a7bff364eece154cd87ba14ad
bailin wei <weibailin@hotmail.com>
2020-12-19 09:24:44 +0800

    add common web project for privacy

---
+++ PrivacyWeb/PrivacyWeb/Pages/Privacy.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Metronome.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+            //@ViewData["Title"]
+            //ViewData["Title"] = "maelzel";
+            //ViewData["Title"] = "test";
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
